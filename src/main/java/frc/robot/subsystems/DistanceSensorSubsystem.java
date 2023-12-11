69e501d3fe7a1369d3bd2c49da1c3ff4a1b9601a
Hakshat Jeyaram <hakshatj@gmail.com>
2023-12-11 10:18:10 -0800

    Update DistanceSensorSubsystem.java

--- src/main/java/frc/robot/subsystems/DistanceSensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/DistanceSensorSubsystem.java
@@ -1,14 +1,24 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.AnalogInput;
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DistanceSensorSubsystem extends SubsystemBase {
   // create your private AnalogPotentiometer instance variable
+  AnalogPotentiometer pot = new AnalogPotentiometer(0,180,30);
   public DistanceSensorSubsystem() {
     // instantiate your sensor
+
   }
   
   // make a getDistance method
+  public double getDistance(){
+    double distance = pot.get();
+    return distance;
+  }
+
 
   @Override
   public void periodic() {
